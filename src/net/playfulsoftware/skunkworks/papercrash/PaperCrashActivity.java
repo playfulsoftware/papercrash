604245c1c96fa5eac933c4ba4bbadeed7d1b104a
Cary Haynie <cary.haynie@gmail.com>
2012-06-03 12:59:17 -0400

    add basic android project

---
+++ src/net/playfulsoftware/skunkworks/papercrash/PaperCrashActivity.java
@@ -0,0 +1,13 @@
+package net.playfulsoftware.skunkworks.papercrash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PaperCrashActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
