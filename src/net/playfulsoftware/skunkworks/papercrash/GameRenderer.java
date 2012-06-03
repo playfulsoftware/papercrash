b3ccdfdc9a8e7a562879dc91644dafbf2389c419
Cary Haynie <cary.haynie@gmail.com>
2012-06-03 17:27:13 -0400

    add stub renderer class

---
+++ src/net/playfulsoftware/skunkworks/papercrash/GameRenderer.java
@@ -0,0 +1,28 @@
+package net.playfulsoftware.skunkworks.papercrash;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class GameRenderer implements Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/net/playfulsoftware/skunkworks/papercrash/PaperCrashActivity.java
+++ src/net/playfulsoftware/skunkworks/papercrash/PaperCrashActivity.java
@@ -1,9 +1,12 @@
 package net.playfulsoftware.skunkworks.papercrash;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class PaperCrashActivity extends Activity {
+	private GLSurfaceView glView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
