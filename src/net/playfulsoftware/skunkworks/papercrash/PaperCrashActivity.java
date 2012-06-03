cb2debf1c3952966698e62546779974504106746
Cary Haynie <cary.haynie@gmail.com>
2012-06-03 19:23:54 -0400

    update layout to include the glsourceview

--- src/net/playfulsoftware/skunkworks/papercrash/PaperCrashActivity.java
+++ src/net/playfulsoftware/skunkworks/papercrash/PaperCrashActivity.java
@@ -5,12 +5,19 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class PaperCrashActivity extends Activity {
-	private GLSurfaceView glView;
+	
+	private GLSurfaceView.Renderer renderer;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        renderer = new GameRenderer();
+        
+        GLSurfaceView glView = (GLSurfaceView) findViewById(R.id.glSurface);
+        
+        glView.setRenderer(renderer);
     }
 }
