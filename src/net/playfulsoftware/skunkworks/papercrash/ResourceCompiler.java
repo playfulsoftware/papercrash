86d4f4d445e18d7edafbc2e6d428099c06f0aa6a
Cary Haynie <cary.haynie@gmail.com>
2012-07-07 14:14:08 -0400

    add texture loader to ResourceCompiler; lint ResourceCompiler.java

--- src/net/playfulsoftware/skunkworks/papercrash/ResourceCompiler.java
+++ src/net/playfulsoftware/skunkworks/papercrash/ResourceCompiler.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import android.app.Activity;
-import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 import android.util.Log;
 
 public class ResourceCompiler implements Closeable {
@@ -18,6 +20,15 @@ public class ResourceCompiler implements Closeable {
 		this.parent = parent;
 	}
 
+	/**
+	 * Builds a shader from the supplied shader source code
+	 *
+	 * @param source
+	 *            Source string for the shader
+	 * @param type
+	 *            Shader type
+	 * @return handle to the compiled shader
+	 */
 	private int buildShader(String source, int type) {
 		// create the shader
 		int shader = GLES20.glCreateShader(type);
@@ -25,35 +36,20 @@ public class ResourceCompiler implements Closeable {
 		GLES20.glShaderSource(shader, source);
 		GLES20.glCompileShader(shader);
 
-		return shader; 
+		return shader;
+	}
+
+	private int createTextureId() {
+		int[] textures = new int[1];
+		GLES20.glGenTextures(1, textures, 0);
+		return textures[0];
 	}
 
 	public int createShader(int shaderId, int shaderType) {
 		try {
 			Log.d("createShader", "Trying to load raw shader file");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(parent.getResources().openRawResource(shaderId)));
-			StringBuffer buffer = new StringBuffer(); 
-			String line = reader.readLine();
-			while (line != null) {
-				buffer.append(line);
-				buffer.append("\n");
-				line = reader.readLine();
-			}
-
-			return buildShader(buffer.toString(), shaderType);
-		} catch (IOException e)
-		{
-			Log.e("GameStart", "Unable to load shader file");
-			return -1;
-		}
-	}
-
-	public int createShader(String fileName, int shaderType) {
-		AssetManager am = parent.getAssets();
-
-		try {
-			Log.d("createShader", "Trying to load raw shader file");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(am.open(fileName, AssetManager.ACCESS_BUFFER)));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					parent.getResources().openRawResource(shaderId)));
 			StringBuffer buffer = new StringBuffer();
 			String line = reader.readLine();
 			while (line != null) {
@@ -63,21 +59,74 @@ public class ResourceCompiler implements Closeable {
 			}
 
 			return buildShader(buffer.toString(), shaderType);
-
 		} catch (IOException e) {
-			Log.e("GameStart", "Unable to load shader file: " + fileName);
+			Log.e("GameStart", "Unable to load shader file");
 			return -1;
 		}
 	}
 
-
-	/** Take a drawable resource id and returns a texture id. */
+	/**
+	 * Takes a resource and returns a texture.
+	 *
+	 * @param id
+	 * @return
+	 */
 	public int createTexture(int id) {
-		return 0;
-	}
+		int texId = createTextureId();
 
-	public int createTexture(String fileName) {
-		return 0;
+		// need to flip the textures vertically.
+		// NOTE: using the non-OpenGl matrix class since we're not doing a 3d
+		// transform on the image (yet).
+		android.graphics.Matrix flip = new android.graphics.Matrix();
+		flip.postScale(1f, -1f);
+
+		// don't scale bitmaps by default.
+		BitmapFactory.Options bmpOpts = new BitmapFactory.Options();
+		bmpOpts.inScaled = false;
+
+		// TODO:: Probably need to do an initial scale to a power of two size to
+		// make everyone happy.
+		// Apparently, not everyone appreciates NPOT textures.
+		Bitmap temp = BitmapFactory.decodeResource(parent.getResources(), id,
+				bmpOpts);
+		Bitmap bmp = Bitmap.createBitmap(temp, 0, 0, temp.getWidth(),
+				temp.getHeight(), flip, true);
+		temp.recycle();
+
+		// setup the OpenGL texture object.
+		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texId);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+				GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR_MIPMAP_NEAREST);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+				GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR_MIPMAP_NEAREST);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
+				GLES20.GL_REPEAT);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,
+				GLES20.GL_REPEAT);
+
+		// generate mipmaps
+		for (int level = 0, width = bmp.getWidth(), height = bmp.getHeight(); true; level++) {
+			// load the texture at the specified mipmap level.
+			GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, level, bmp, 0);
+
+			// don't need to go lower than a single pixel mipmap
+			if (width == 1 && height == 1)
+				break;
+
+			// scale the bitmap down for the next pass.
+			width >>= 1;
+			height >>= 1;
+			width = (width < 1) ? 1 : width;
+			height = (height < 1) ? 1 : height;
+
+			Bitmap bmp2 = Bitmap.createScaledBitmap(bmp, width, height, true);
+			bmp.recycle();
+			bmp = bmp2;
+		}
+
+		bmp.recycle();
+
+		return texId;
 	}
 
 	@Override
